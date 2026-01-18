b47fc6b23fc2a12ccd2dbcb1a7c6a0a86045ade0
Logan Bleyl <loganmbleyl05@gmail.com>
2026-01-18 06:51:21 -0500

    Discord bot tutorial

---
+++ main.py
@@ -0,0 +1,84 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+default_role = "𝕿𝖆𝖗𝖓𝖎𝖘𝖍𝖊𝖉"
+
+@bot.event
+async def on_ready():
+    print(f"we are ready to go in, {bot.user.name}")
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - don't use that word")
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def poll(ctx, *, question):
+    embed = discord.Embed(title="New Poll", description=question)
+    poll_message = await ctx.send(embed=embed)
+    await poll_message.reaction(":Thugshaker:")
+    await poll_message.reaction(":Jman:")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+@bot.command()
+async def assign(ctx):
+    role = discord.utils.get(ctx.guild.roles, name=default_role)
+    if role:
+        await ctx.author.add_roles(role)
+        await ctx.send(f"{ctx.author.mention} is now assigned to {default_role}.")
+    await ctx.send("Role doesn't exist")
+
+@bot.command()
+async def remove(ctx):
+    role = discord.utils.get(ctx.guild.roles, name=default_role)
+    if role:
+        await ctx.author.remove_roles(role)
+        await ctx.send(f"{default_role} is now removed from {ctx.author.mention}.")
+    await ctx.send("Role doesn't exist")
+
+@bot.command()
+async def dm(ctx, *, msg):
+    await ctx.author.send(f"You said {msg}.")
+
+@bot.command()
+async def reply(ctx):
+    await ctx.reply("This is a reply to your message.")
+
+@bot.command()
+@commands.has_role(default_role)
+async def secret(ctx):
+    await ctx.send("Welcome Tarnished.")
+
+@secret.error
+async def secret_error(ctx, error):
+    if isinstance(error, commands.MissingRole):
+        await ctx.send("You don't have permissions to that.")
+
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+
